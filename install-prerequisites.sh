d53713ff00923aec05046fa29524373fd3a4ae9d
yusuf maral <46676098+maralyusuf@users.noreply.github.com>
2021-02-03 01:37:11 +0300

    Update install-prerequisites.sh

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -5,10 +5,12 @@ sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt-get install libatlas-base-dev
 sudo apt-get install libjasper-dev
 
+# Install the tflite_runtime
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+
 # Installs OpenCV pip package
 
-pip install opencv-python==4.1.0.25
+pip3 install opencv-python==4.1.0.25
 
-# Install the tflite_runtime
-pip install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+
 echo Prerequisites Installed Successfully
